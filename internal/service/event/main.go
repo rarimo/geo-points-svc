55408181790d7a2594265c4c3979ab4817c17fd2
trpdjke <yehorushakov@gmail.com>
2024-08-06 17:56:42 +0300

    added autoClaimEventsForBalance to balance when using the script to reward for testing

--- internal/service/event/main.go
+++ internal/service/event/main.go
@@ -5,16 +5,22 @@ import (
 
 	"github.com/rarimo/geo-points-svc/internal/config"
 	"github.com/rarimo/geo-points-svc/internal/data"
+	"github.com/rarimo/geo-points-svc/internal/data/evtypes"
 	"github.com/rarimo/geo-points-svc/internal/data/evtypes/models"
 	"github.com/rarimo/geo-points-svc/internal/data/pg"
+	"github.com/rarimo/geo-points-svc/internal/service/handlers"
+	"gitlab.com/distributed_lab/logan/v3"
 )
 
 func Run(cfg config.Config, date int) error {
 	log := cfg.Log()
 	db := cfg.DB()
+	lvls := cfg.Levels()
+	evTypes := cfg.EventTypes()
 
 	balancesQ := pg.NewBalances(db)
-	events := pg.NewEvents(db)
+	eventsQ := pg.NewEvents(db)
+	referralsQ := pg.NewReferrals(db)
 
 	balances, err := balancesQ.FilterByCreatedBefore(date).FilterVerified().Select()
 
@@ -28,9 +34,9 @@ func Run(cfg config.Config, date int) error {
 		return nil
 	}
 
-	existingEvents, err := events.FilterByType(models.TypeEarlyTest).Select()
+	existingEvents, err := eventsQ.FilterByType(models.TypeEarlyTest).Select()
 	if err != nil {
-		log.WithError(err).Error("failed to filter events")
+		log.WithError(err).Error("failed to filter eventsQ")
 		return err
 	}
 
@@ -39,17 +45,83 @@ func Run(cfg config.Config, date int) error {
 		existingEventsMap[event.Nullifier] = event
 	}
 
-	for _, user := range balances {
-		if _, exists := existingEventsMap[user.Nullifier]; !exists {
-			err = events.Insert(data.Event{
-				Nullifier: user.Nullifier,
-				Type:      models.TypeEarlyTest,
-				Status:    data.EventFulfilled,
-			})
-			if err != nil {
-				return fmt.Errorf("failed to insert `early_test` event: %w", err)
-			}
+	for _, balance := range balances {
+		if _, exists := existingEventsMap[balance.Nullifier]; exists {
+			continue
 		}
+		err = eventsQ.Insert(data.Event{
+			Nullifier: balance.Nullifier,
+			Type:      models.TypeEarlyTest,
+			Status:    data.EventFulfilled,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to insert `early_test` event: %w", err)
+		}
+		err = autoClaimEventsForBalance(*log, eventsQ, balancesQ, &balance, lvls, referralsQ, *evTypes)
+		if err != nil {
+			return fmt.Errorf("failed to auto-claim eventsQ: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func autoClaimEventsForBalance(
+	log logan.Entry, eventsQ data.EventsQ, balanceQ data.BalancesQ, balance *data.Balance,
+	lvls *config.Levels, referralsQ data.ReferralsQ, evTypes evtypes.Types) error {
+
+	if balance == nil {
+		log.Debug("Balance absent. Events not claimed.")
+		return nil
+	}
+
+	if balance.IsDisabled() || !balance.IsVerified() {
+		log.Debug("User not eligible for event claiming. Events not claimed.")
+		return nil
+	}
+
+	var totalPoints int64
+	eventsToClaim, err := eventsQ.
+		FilterByNullifier(balance.Nullifier).
+		FilterByStatus(data.EventFulfilled).
+		Select()
+	if err != nil {
+		return fmt.Errorf("failed to select events for user=%s: %w", balance.Nullifier, err)
+	}
+
+	eventsMap := map[string][]string{}
+	for _, e := range eventsToClaim {
+		if _, ok := eventsMap[e.Type]; !ok {
+			eventsMap[e.Type] = []string{}
+		}
+		eventsMap[e.Type] = append(eventsMap[e.Type], e.ID)
+	}
+
+	for evName, evIDs := range eventsMap {
+		evType := evTypes.Get(evName, evtypes.FilterInactive, evtypes.FilterByAutoClaim(true))
+		if evType == nil {
+			continue
+		}
+
+		_, err = eventsQ.FilterByID(evIDs...).Update(data.EventClaimed, nil, &evType.Reward)
+		if err != nil {
+			return fmt.Errorf("failedt to update %s events for user=%s: %w", evName, balance.Nullifier, err)
+		}
+
+		totalPoints += evType.Reward * int64(len(evIDs))
+	}
+
+	level, err := handlers.DoLevelRefUpgrade(lvls, referralsQ, balance, totalPoints)
+	if err != nil {
+		return fmt.Errorf("failed to do lvlup and referrals updates: %w", err)
+	}
+
+	err = balanceQ.FilterByNullifier(balance.Nullifier).Update(map[string]any{
+		data.ColAmount: pg.AddToValue(data.ColAmount, totalPoints),
+		data.ColLevel:  level,
+	})
+	if err != nil {
+		return fmt.Errorf("update balance amount and level: %w", err)
 	}
 
 	return nil
