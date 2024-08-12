2e166959fa89aab0faa0c68a95df36c76b67df4b
trpdjke <yehorushakov@gmail.com>
2024-08-12 13:29:31 +0300

    add migrate file for daily_questions table

---
+++ internal/assets/migrations/004_daily_questions_tabel.sql
@@ -0,0 +1,11 @@
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS daily_questions (
+     id SERIAL PRIMARY KEY,
+     title VARCHAR(255) NOT NULL,
+     time_for_answer INTEGER NOT NULL,
+     bounty INTEGER NOT NULL,
+     answer_options JSONB NOT NULL
+);
+
+-- +migrate Down
+DROP TABLE IF EXISTS daily_questions;
