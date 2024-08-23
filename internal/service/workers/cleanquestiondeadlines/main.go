7e3c56b5e4137ba07c1369c6fc377061b99a1f72
trpdjke <yehorushakov@gmail.com>
2024-08-23 14:02:28 +0300

    edit daily questions cleaner

--- internal/service/workers/cleanquestiondeadlines/main.go
+++ internal/service/workers/cleanquestiondeadlines/main.go
@@ -2,40 +2,45 @@ package cleanquestiondeadlines
 
 import (
 	"context"
-	"time"
+	"fmt"
 
+	"github.com/go-co-op/gocron/v2"
 	"github.com/rarimo/geo-points-svc/internal/config"
+	"github.com/rarimo/geo-points-svc/internal/service/workers/cron"
 )
 
 func Run(ctx context.Context, cfg config.Config, sig chan struct{}) {
 	offset := cfg.DailyQuestions().Timezone
+	if offset < 0 {
+		offset = 12 + offset
+	}
+	cron.Init(cfg.Log())
 
-	for {
-		now := time.Now().UTC().Add(time.Duration(offset) * time.Hour)
-		cfg.Log().Info("Daily Question cleaning start")
-		nextMidnight := time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, time.UTC).
-			Add(time.Duration(offset) * time.Hour)
+	atDayStart := gocron.NewAtTimes(gocron.NewAtTime(uint(offset), 0, 0))
 
-		durationUntilNextTick := nextMidnight.Sub(now)
+	_, err := cron.NewJob(
+		gocron.DailyJob(1, atDayStart),
+		gocron.NewTask(job, cfg, ctx, sig),
+		gocron.WithName("Daily Questions leaner"),
+	)
+	if err != nil {
+		panic(fmt.Errorf("cleaner daily questions: failed to initialize daily job: %w", err))
+	}
 
-		timer := time.NewTimer(durationUntilNextTick)
+}
 
-		select {
-		case <-timer.C:
-			res := cfg.DailyQuestions().ClearDeadlines()
-			cfg.Log().Infof("Сleared daily questions quantity: %v", res)
+func job(cfg config.Config, ctx context.Context, sig chan struct{}) {
+	select {
+	default:
+		res := cfg.DailyQuestions().ClearDeadlines()
+		cfg.Log().Infof("Сleared daily questions quantity: %v", res)
 
-			timer.Stop()
+	case <-sig:
+		cfg.Log().Info("Daily Question cleaning stop")
+		return
 
-		case <-sig:
-			cfg.Log().Info("Daily Question cleaning stop")
-			timer.Stop()
-			return
-
-		case <-ctx.Done():
-			cfg.Log().Info("Daily Question cleaning stop")
-			timer.Stop()
-			return
-		}
+	case <-ctx.Done():
+		cfg.Log().Info("Daily Question cleaning stop")
+		return
 	}
 }
